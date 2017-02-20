bde40e39363d43e2b72aab83c6d739caffc51469
Stefan Wille <github@stefanwille.com>
2017-02-20 15:58:09 -0300

    Add a basic server

---
+++ src/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+const port = 8080;
+const app = express();
+
+// Don't show the log when in test env
+if(process.env['NODE_ENV'] === 'test') {
+    // Use morgan to log to command line
+    app.use(morgan('combined'));
+}
+
+// Parse application/json and look for raw text
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/json'}));
+
+app.get("/", (req, res) => res.json({message: "Welcome to our Bookstore!"}));
+
+app.listen(port);
+console.log("Listening on port " + port);
+
+module.exports = app; // for testing
