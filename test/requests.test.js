f8dfcbb4256f39d17d80103aa0ce14e214b8a964
Stefan Wille <github@stefanwille.com>
2017-02-21 16:32:05 -0300

    Rename books test

--- test/requests.test.js
+++ test/requests.test.js
@@ -3,10 +3,13 @@ process.env.NODE_ENV = 'test'
 const chai = require('chai')
 const chaiHttp = require('chai-http')
 const expect = chai.expect
+const models = require('../src/models')
 
 const server = require('../src/server')
 
 chai.use(chaiHttp)
+
+resetDatabase
 
 describe('/books', function() {
   describe('GET /', function() {
