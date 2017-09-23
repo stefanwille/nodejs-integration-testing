faa12d52d83da5d2c962d303aa1f198da67c3c67
Stefan Wille <github@stefanwille.com>
2017-09-23 12:52:49 +0200

    Arrow functions

--- test/requests.test.js
+++ test/requests.test.js
@@ -1,104 +1,104 @@
-'use strict'
+'use strict';
 
-const chai = require('chai')
-const expect = chai.expect
-const request = require('supertest')
+const chai = require('chai');
+const expect = chai.expect;
+const request = require('supertest');
 
-const server = require('../src/server')
-const { resetDatabase, Book } = require('../src/models')
+const server = require('../src/server');
+const { resetDatabase, Book } = require('../src/models');
 
-describe('/books requests', function() {
-  beforeEach(resetDatabase)
+describe('/books requests', () => {
+  beforeEach(resetDatabase);
 
-  describe('GET /books', function() {
-    beforeEach(async function() {
-      await Book.create({ title: 'Some title', author: 'Algun author' })
-    })
+  describe('GET /books', () => {
+    beforeEach(async () => {
+      await Book.create({ title: 'Some title', author: 'Algun author' });
+    });
 
-    it('works', async function() {
-      const response = await request(server).get('/books')
-      expect(response.body.books).to.deep.equal([{ title: 'Some title', author: 'Algun author' }])
-    })
-  })
+    it('works', async () => {
+      const response = await request(server).get('/books');
+      expect(response.body.books).to.deep.equal([{ title: 'Some title', author: 'Algun author' }]);
+    });
+  });
 
-  describe('GET /books/:id', function() {
-    let book
+  describe('GET /books/:id', () => {
+    let book;
 
-    beforeEach(async function() {
-      book = await Book.create({ title: 'Other title', author: 'Other author' })
-    })
+    beforeEach(async () => {
+      book = await Book.create({ title: 'Other title', author: 'Other author' });
+    });
 
-    it('works', async function() {
-      const response = await request(server).get(`/books/${book.id}`)
-      expect(response.body.book).to.deep.equal({ title: 'Other title', author: 'Other author' })
-    })
+    it('works', async () => {
+      const response = await request(server).get(`/books/${book.id}`);
+      expect(response.body.book).to.deep.equal({ title: 'Other title', author: 'Other author' });
+    });
 
-    it('returns status 404 when not found', async function() {
-      const response = await request(server).get('/books/12345')
-      expect(response.status).to.equal(404)
-      expect(response.body).to.deep.equal({ error: 'NOT_FOUND' })
-    })
-  })
+    it('returns status 404 when not found', async () => {
+      const response = await request(server).get('/books/12345');
+      expect(response.status).to.equal(404);
+      expect(response.body).to.deep.equal({ error: 'NOT_FOUND' });
+    });
+  });
 
-  describe('POST /books (= create)', function() {
-    it('works', async function() {
+  describe('POST /books (= create)', () => {
+    it('works', async () => {
       const response = await request(server)
         .post('/books')
-        .send({ book: { title: 'Creator title', author: 'Creator author' } })
+        .send({ book: { title: 'Creator title', author: 'Creator author' } });
       // eslint-disable-next-line no-unused-expressions
-      expect(response.body.book.id).not.to.be.null
+      expect(response.body.book.id).not.to.be.null;
       expect(response.body.book).to.deep.include({
         title: 'Creator title',
         author: 'Creator author',
-      })
-      const loadedBook = await Book.findById(response.body.book.id)
+      });
+      const loadedBook = await Book.findById(response.body.book.id);
       expect(loadedBook).to.deep.include({
         title: 'Creator title',
         author: 'Creator author',
-      })
-    })
-  })
+      });
+    });
+  });
 
-  describe('PATCH /books/:id (= update)', function() {
-    let book
+  describe('PATCH /books/:id (= update)', () => {
+    let book;
 
-    beforeEach(async function() {
-      book = await Book.create({ title: 'Original title', author: 'Original author' })
-    })
+    beforeEach(async () => {
+      book = await Book.create({ title: 'Original title', author: 'Original author' });
+    });
 
-    it('works', async function() {
+    it('works', async () => {
       const response = await request(server)
         .patch(`/books/${book.id}`)
-        .send({ book: { title: 'Updated title' } })
-      expect(response.status).to.equal(200)
+        .send({ book: { title: 'Updated title' } });
+      expect(response.status).to.equal(200);
       expect(response.body.book).to.deep.include({
         title: 'Updated title',
         author: 'Original author',
-      })
-      await book.reload()
+      });
+      await book.reload();
       expect(book).to.deep.include({
         title: 'Updated title',
         author: 'Original author',
-      })
-    })
-  })
+      });
+    });
+  });
 
-  describe('DELETE /books/:id (= destroy)', function() {
-    let book
+  describe('DELETE /books/:id (= destroy)', () => {
+    let book;
 
-    beforeEach(async function() {
-      book = await Book.create({ title: 'Original title', author: 'Original author' })
-    })
+    beforeEach(async () => {
+      book = await Book.create({ title: 'Original title', author: 'Original author' });
+    });
 
-    it('works', async function() {
+    it('works', async () => {
       const response = await request(server)
         .delete(`/books/${book.id}`)
-        .send({ book: { title: 'Updated title' } })
-      expect(response.status).to.equal(200)
-      expect(response.body).to.deep.equal({})
-      const loadedBook = await Book.findById(book.id)
+        .send({ book: { title: 'Updated title' } });
+      expect(response.status).to.equal(200);
+      expect(response.body).to.deep.equal({});
+      const loadedBook = await Book.findById(book.id);
       // eslint-disable-next-line no-unused-expressions
-      expect(loadedBook).to.be.null
-    })
-  })
-})
+      expect(loadedBook).to.be.null;
+    });
+  });
+});
